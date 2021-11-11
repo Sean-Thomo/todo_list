54b6771eb321abb3a5082de813d100949221e0a3
Sean-Thomo <seansthomo@gmail.com>
2021-11-11 13:32:18 +0200

    added code to redirect list

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
 let items =[];
+let workItems =[];
 
 app.get("/", (req, res) => {
 
@@ -31,9 +32,29 @@ app.get("/", (req, res) => {
 
 app.post('/', (req, res) => {
   item = req.body.newItem;
-  items.push(item);
-  res.redirect('/');
+
+  if (req.body.list == 'Work'){
+    workItems.push(item)
+    res.redirect('/work')
+  } else {
+    items.push(item);
+    res.redirect('/')
+  }
+
 });
+
+app.get('/work', (req, res) => {
+  res.render('list', {
+    listTitle: 'Work List',
+    newListItems: workItems
+  })
+})
+
+app.post('/work', (req, res) => {
+  let item = req.body.newItem;
+  workItems.push(item);
+  res.redirect('/work')
+})
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
