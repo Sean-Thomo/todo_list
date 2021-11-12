c5402ab60f768cc1a3d17fff3a7b15edbf478fe7
Sean-Thomo <seansthomo@gmail.com>
2021-11-12 15:53:49 +0200

    added express routes for custom params

--- date.js
+++ date.js
@@ -1,13 +1,12 @@
-module.exports = getDate
+exports.getDate = function(){
 
-function getDate(){
-  let options = {
+  const today = new Date();
+
+  const options = {
     weekday: 'long',
     day: 'numeric',
     month: 'long'
   };
 
-  let day = today.toLocaleDateString('en-US', options);
-
-  return day
+  return today.toLocaleDateString('en-US', options);
 }
